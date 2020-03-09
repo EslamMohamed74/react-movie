28a29dae0136118047d664df2c1257c7ef32426a
Eslam Mohamed <eslamm7amed123@gmail.com>
2020-03-09 21:02:15 +0200

    Updates

---
+++ precache-manifest.ee79414c76f3f4b84e1e75d0fd414fe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6e69d1e716d962ad3a6432f1c81414",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "26b5ee34b27eaede1222",
+    "url": "/react-movie/static/css/main.f9859e44.chunk.css"
+  },
+  {
+    "revision": "394f2605e5c9248e6645",
+    "url": "/react-movie/static/js/2.a25e41fd.chunk.js"
+  },
+  {
+    "revision": "26b5ee34b27eaede1222",
+    "url": "/react-movie/static/js/main.267d502d.chunk.js"
+  },
+  {
+    "revision": "c16bae6f0d187d038fb1",
+    "url": "/react-movie/static/js/runtime~main.9b5b6b5c.js"
+  }
+]);
